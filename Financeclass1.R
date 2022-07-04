24a86738b15b55dd1754d5dc83472e6ff1746cf6
ShareableSolutions <108496222+ShareableSolutions@users.noreply.github.com>
2022-07-04 16:33:14 -0300

    Add files via upload

---
+++ Financeclass1.R
@@ -0,0 +1,12 @@
+#Install the following packages to work with time series 
+
+install.packages("forecast")
+install.packages("h2o")
+install.packages("TSstudio")
+install.packages("plotly")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("lubridate")
+install.packages("xts")
+install.packages("zoo")
+install.packages("UKgrid")
